8669a6f68046031a5d9d0139ccb5eca4892ad10a
Kaylin Shioshita <kaylin.shioshita@colorado.edu>
2017-01-31 11:03:11 -0700

    added css and js

---
+++ js/lovebash.js
@@ -0,0 +1,7 @@
+document.getElementById("man").addEventListener("mouseover", man);
+// document.getElementById("man").addEventListener("mousekeave", reset);
+function man(){
+	document.getElementById("man").syle.backgroundColor="red";
+
+}
+
